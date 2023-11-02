fde8b7d74cbcfd243b4a34eaaa489abe8acf3308
dmtrack <rightrack888@gmail.com>
2023-11-02 11:36:02 +0500

    feat: cart message

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -2,15 +2,24 @@ import styles from './Cart.module.scss';
 import React from 'react';
 import CardItem from '@/components/CartItem/CartItem';
 import { useAppSelector } from '@/store/useRedux';
+import Typography from '@/components/Typography';
 const { v4: uuidv4 } = require('uuid');
 
 function CartPage() {
     const items = useAppSelector((state) => state.cart.cartItems);
 
     return (
-        <div className={styles.section}>
-            {items && items.map((p) => <CardItem key={uuidv4()} {...p} />)}
-        </div>
+        <>
+            {!items.length && (
+                <Typography variant='h4' className={styles.message}>
+                    Ваша корзина пуста
+                </Typography>
+            )}
+
+            <div className={styles.section}>
+                {items && items.map((p) => <CardItem key={uuidv4()} {...p} />)}
+            </div>
+        </>
     );
 }
 
